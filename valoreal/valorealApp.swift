353262e099fce6047c60aae3a42a2414646b3f03
Aansh Singh <singhsitanshu2006@gmail.com>
2026-03-22 16:05:41 -0500

    Initial Commit

---
+++ valoreal/valorealApp.swift
@@ -0,0 +1,17 @@
+//
+//  valorealApp.swift
+//  valoreal
+//
+//  Created by Sitanshu Singh on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct valorealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
